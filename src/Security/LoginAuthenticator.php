b615c268950f8e4edf66fd5a06589080e10903a1
Fabian Nino <fabian@nailharmony.com>
2025-08-18 01:02:26 -0700

    completed login

---
+++ src/Security/LoginAuthenticator.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class LoginAuthenticator extends AbstractLoginFormAuthenticator
+{
+    use TargetPathTrait;
+
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        private KernelInterface $kernel,
+    )
+    {
+    }
+    /**
+     * @inheritDoc
+     */
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate('app_login');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function authenticate(Request $request): Passport
+    {
+        $credentials = [
+            'username' => $request->request->get('_username', ''),
+            'password' => $request->request->get('_password', ''),
+            'csrf_token' => $request->request->get('_csrf_token'),
+        ];
+
+        $request->getSession()->set(
+            SecurityRequestAttributes::LAST_USERNAME,
+            $credentials['username']
+        );
+
+        $httpClientParams = [];
+
+        if ($this->kernel->getEnvironment() === 'dev') {
+            $httpClientParams = [
+                'verify_peer' => false,
+                'verify_host' => false,
+            ];
+        }
+
+        $httpClient = HttpClient::create($httpClientParams);
+
+        $myNAUserReq = $httpClient->request('POST', 'http://127.0.0.1:8000/remote/login', [
+            'json' => [
+                'security' => [
+                    'credentials' => [
+                        'login' => $credentials['username'],
+                        'password' => $credentials['password'],
+                    ]
+                ]
+            ]
+        ]);
+
+        $passport = new SelfValidatingPassport(
+            new UserBadge($credentials['username'], function ($identifier) use ($myNAUserReq) {
+                if ($myNAUserReq->getStatusCode() !== Response::HTTP_OK) {
+                    throw new AuthenticationException("Incorrect username or password");
+                }
+
+                $myNAUser = $myNAUserReq->toArray();
+                $user = new User();
+                $user->setUsername($identifier);
+                $user->setRoles($myNAUser['roles']);
+                return $user;
+            }),
+            [
+                new CsrfTokenBadge('authenticate', $credentials['csrf_token']),
+                new RememberMeBadge(),
+            ]
+        );
+
+        return $passport;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // 3. Check for a saved target path
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        // 4. If no target path, redirect to a default route
+        return new RedirectResponse($this->urlGenerator->generate('home'));
+    }
+}
---
+++ src/Security/User.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface
+{
+    private $username;
+
+    /**
+     * @var list<string> The user roles
+     */
+    private $roles = [];
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
+     */
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+        // $data["\0".self::class."\0password"] = hash('crc32c', $this->password);
+
+        return $data;
+    }
+
+    #[\Deprecated]
+    public function eraseCredentials(): void
+    {
+        // @deprecated, to be removed when upgrading to Symfony 8
+    }
+}
