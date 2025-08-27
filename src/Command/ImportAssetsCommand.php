b4bd7afcd788867d656d08c96c86c8115c1f1b9d
Fabian Nino <fabian@nailharmony.com>
2025-08-26 22:02:57 -0700

    test

--- src/Command/ImportAssetsCommand.php
+++ src/Command/ImportAssetsCommand.php
@@ -80,8 +80,10 @@ class ImportAssetsCommand extends Command
             $io->success('Asset import completed successfully.');
 
         } catch (IOException $e) {
+            dd("IO", $e);
             $io->error($e->getMessage());
         } catch (\Exception $e) {
+            dd("Exception", $e);
             $io->error('An error occurred during the import process: ' . $e->getMessage());
             return Command::FAILURE;
         }
